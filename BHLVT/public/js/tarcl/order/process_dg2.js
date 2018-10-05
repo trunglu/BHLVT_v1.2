2860c2e5044767d13c589d147ac65635b653727e
the_dead29 <trunglu885@gmail.com>
2018-10-05 13:08:01 +0700

    ()()====+++

---
+++ BHLVT/public/js/tarcl/order/process_dg2.js
@@ -0,0 +1,14 @@
+﻿var dg2 = new Datagrid("#donhang_index_dg2");
+function onClickCellDg2(index, field) {
+    dg2.onClickCell(index, field);
+}
+function onChangeMaVtDg2(newValue, oldValue) {
+    if (newValue !== oldValue) {
+        var ed = $(dg2.selectorDg).datagrid('getEditor', { index: dg2.editIndex, field: "IDItem" });
+        if (ed != null && typeof (ed.target) !== "undefined") {
+            //Xử lý bên client khi nguời dùng thay đổi mã vật tư
+            console.log("change giá trị mã vt, giá trị mới: ");
+            console.log($(ed.target).val());
+        }
+    }
+}
